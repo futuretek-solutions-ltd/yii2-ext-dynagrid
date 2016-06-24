2a08e06831d076fa2ec11738f3e3d08ea8862266
Lukáš Černý <lukas.cerny@futuretek.cz>
2016-06-24 18:54:39 +0200

    Zmena view, aby se dotahovaly z puvodniho modulu

--- DynaGrid.php
+++ DynaGrid.php
@@ -4,4 +4,14 @@ namespace futuretek\dynagrid;
 
 class DynaGrid extends \kartik\dynagrid\DynaGrid
 {
+    /**
+     * @inheritDoc
+     */
+    protected function initGrid()
+    {
+        $this->_module->configView = '@kartik/dynagrid/views/config.php';
+        $this->_module->settingsView = '@kartik/dynagrid/views/settings.php';
+
+        parent::initGrid();
+    }
 }
